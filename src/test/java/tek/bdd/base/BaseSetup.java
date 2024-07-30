ff2b77fe352f252cb5ce3aa9f43a415f9ab74792
zianzahid <ziamustafa2000@hotmail.com>
2024-07-30 16:38:02 -0700

    setup class complete

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,35 @@
+package tek.bdd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseSetup {
+
+    //Encapsulating driver instance
+    private static WebDriver driver;
+    public void setupBrowser( ){
+        /*
+        Web Driver instance
+        Navigation to Retail
+        maximize
+        and implicitly wait
+         */
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+    public void quitBrowser(){
+
+        if (driver !=null){
+            driver.quit();
+        }
+
+    }
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+}
