2d633e389a99f31fe0c6b1077fa49a466dd534b5
zianzahid <ziamustafa2000@hotmail.com>
2024-07-31 18:53:44 -0700

    SeleniumUtility class and activity

---
+++ src/test/java/tek/bdd/pages/SignInPage.java
@@ -0,0 +1,16 @@
+package tek.bdd.pages;
+
+import org.openqa.selenium.By;
+
+public class SignInPage {
+
+    public static final By PAGE_SUBTITLE = By.className("login__subtitle");
+
+    public static final By EMAIL_INPUT = By.name("email");
+
+    public static final By PASSWORD_INPUT = By.name("password");
+
+    public static final By LOGIN_BUTTON = By.id("loginBtn");
+
+    public static final By ERROR_MESSAGE = By.className("error");
+}
