308d2e1db62bf33b36658bc2b541a009ab10baba
zianzahid <ziamustafa2000@hotmail.com>
2024-08-08 16:56:46 -0700

    class and activity August 8

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -1,7 +1,14 @@
 package tek.bdd.base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,10 +17,10 @@ import java.time.Duration;
 import java.util.Properties;
 
 public abstract class BaseSetup {
-
+    private static final Logger LOGGER = LogManager.getLogger(BaseSetup.class);
     //Encapsulating driver instance
     private static WebDriver driver;
-    private Properties properties;
+    private final Properties properties;
 
     //Find the full path to file
     //FileInputStream to read the file
@@ -23,18 +30,43 @@ public abstract class BaseSetup {
         try {
             String configFilePath = System.getProperty("user.dir") +
                     "/src/test/resources/configs/dev-config.properties";
+            LOGGER.info("Reading Config file " + configFilePath);
             File file = new File(configFilePath);
             FileInputStream fileInputStream = new FileInputStream(file);
             properties = new Properties();
             properties.load(fileInputStream);
-        }catch (IOException ex) {
+        } catch (IOException ex) {
+            LOGGER.error("Error reading config file", ex);
             throw new RuntimeException("Something wrong with Config file", ex);
         }
     }
 
     public void setupBrowser() {
-        driver = new ChromeDriver();
+        //To Open Chrome browser in headless mode
+        String browserType = properties.getProperty("ui.browser");
+        boolean isHeadless = Boolean.parseBoolean(properties.getProperty("ui.browser.headless"));
+        LOGGER.info("Running on browser {} and isHeadless {}", browserType, isHeadless);
+        if (browserType.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            if (isHeadless)
+                options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        } else if (browserType.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            if (isHeadless) options.addArguments("--headless");
+            driver = new EdgeDriver(options);
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            if (isHeadless)
+                options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
+        } else {
+            throw new RuntimeException("Wrong browser type choose between chrome, firefox or edge");
+        }
+
+
         String url = properties.getProperty("ui.url");
+        LOGGER.debug("Using URL {}", url);
         driver.get(url);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
--- src/test/java/tek/bdd/steps/CreateNewAccountSteps.java
+++ src/test/java/tek/bdd/steps/CreateNewAccountSteps.java
@@ -3,6 +3,7 @@ package tek.bdd.steps;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.hu.Ha;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import tek.bdd.pages.AccountPage;
@@ -13,44 +14,50 @@ import tek.bdd.utility.SeleniumUtility;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class CreateNewAccountSteps extends SeleniumUtility {
     private static String emailToUse;
 
-    @When("user enter {string}, {string}, and {string}")
+    @When("user enter {string} and {string}  and {string}")
     public void userEnterNewAccountInfo(String name, String email, String password) {
+
         emailToUse = email.equalsIgnoreCase("random")
-                ? RandomGenerator.generateRandomEmail()
-                : email;
+                ? RandomGenerator.generateRandomEmail() : email;
 
         sendText(SignUpPage.NAME_INPUT, name);
         sendText(SignUpPage.EMAIL_INPUT, emailToUse);
         sendText(SignUpPage.PASSWORD_INPUT, password);
         sendText(SignUpPage.CONFIRM_PASSWORD, password);
+
     }
 
     @Then("validate user is in account page")
     public void validateUserInAccountPage() {
         String actualText = getElementText(AccountPage.PROFILE_PAGE_TITLE);
-        Assert.assertEquals("Account page should contain 'Your Profile' text", "Your Profile", actualText);
+
+        Assert.assertEquals("Account page should contains Your Profile Text"
+                , "Your Profile", actualText);
     }
 
     @Then("validate email address in account page match")
     public void validateEmailAddressInAccountPageMatch() {
         String actualEmail = getElementText(AccountPage.PROFILE_EMAIL_TEXT);
-        Assert.assertEquals("Email in account page should match with email used in create account step", emailToUse, actualEmail);
+
+        Assert.assertEquals("Email in Account page should match with email used in create account step",
+                emailToUse, actualEmail);
     }
 
     @When("user enter new account info")
     public void user_enter_new_account_info(DataTable dataTable) {
-        Map<String, String> data = dataTable.asMap(String.class, String.class);
+        //Converting data table to Map<String, String>
+        Map<String, String> data = dataTable.asMap();
         String email = data.get("email");
         String name = data.get("name");
         String password = data.get("password");
 
         emailToUse = email.equalsIgnoreCase("random")
-                ? RandomGenerator.generateRandomEmail()
-                : email;
+                ? RandomGenerator.generateRandomEmail() : email;
 
         sendText(SignUpPage.NAME_INPUT, name);
         sendText(SignUpPage.EMAIL_INPUT, emailToUse);
@@ -60,14 +67,14 @@ public class CreateNewAccountSteps extends SeleniumUtility {
 
     @When("user enter new account info using list Data")
     public void user_enter_new_account_info_using_list_data(DataTable dataTable) {
-        List<String> data = dataTable.asList(String.class);
+        //Convert Data Table to List.
+        List<String> data = dataTable.asList();
         String name = data.get(0);
         String email = data.get(1);
         String password = data.get(2);
 
         emailToUse = email.equalsIgnoreCase("random")
-                ? RandomGenerator.generateRandomEmail()
-                : email;
+                ? RandomGenerator.generateRandomEmail() : email;
 
         sendText(SignUpPage.NAME_INPUT, name);
         sendText(SignUpPage.EMAIL_INPUT, emailToUse);
@@ -77,14 +84,14 @@ public class CreateNewAccountSteps extends SeleniumUtility {
 
     @When("user enter new account as list of list")
     public void user_enter_new_account_as_list_of_list(DataTable dataTable) {
-        List<List<String>> data = dataTable.asLists(String.class);
+        //convert data table to List of List
+        List<List<String>> data = dataTable.asLists();
         String name = data.get(0).get(0);
         String email = data.get(0).get(1);
         String password = data.get(0).get(2);
 
         emailToUse = email.equalsIgnoreCase("random")
-                ? RandomGenerator.generateRandomEmail()
-                : email;
+                ? RandomGenerator.generateRandomEmail() : email;
 
         sendText(SignUpPage.NAME_INPUT, name);
         sendText(SignUpPage.EMAIL_INPUT, emailToUse);
@@ -94,14 +101,15 @@ public class CreateNewAccountSteps extends SeleniumUtility {
 
     @When("user enter new account as list of maps")
     public void user_enter_new_account_as_list_of_maps(DataTable dataTable) {
-        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
+        //Converting data table to list of Maps
+        List<Map<String, String>> data = dataTable.asMaps();
+
         String email = data.get(0).get("email");
         String name = data.get(0).get("name");
         String password = data.get(0).get("password");
 
         emailToUse = email.equalsIgnoreCase("random")
-                ? RandomGenerator.generateRandomEmail()
-                : email;
+                ? RandomGenerator.generateRandomEmail() : email;
 
         sendText(SignUpPage.NAME_INPUT, name);
         sendText(SignUpPage.EMAIL_INPUT, emailToUse);
@@ -111,35 +119,61 @@ public class CreateNewAccountSteps extends SeleniumUtility {
 
     @Then("validate field error messages")
     public void validate_field_error_messages(DataTable dataTable) {
-        List<String> expectedData = dataTable.asList(String.class);
-        List<WebElement> errorElements = getElements(SignUpPage.ERROR_MESSAGE);
+        // Converting data table to list
+        List<String> expectedData = dataTable.asList();
 
-        Assert.assertEquals("Number of error messages should be the same as expected", expectedData.size(), errorElements.size());
+        //GetActual Data
+        List<WebElement> errorElements = getElements(SignUpPage.ERROR_MESSAGE);
+        Assert.assertEquals("Number of Error message should be same as Expected",
+                expectedData.size(),
+                errorElements.size());
 
         for (int index = 0; index < expectedData.size(); index++) {
             String expected = expectedData.get(index);
             String actual = errorElements.get(index).getText();
 
-            Assert.assertEquals("Error message should match", expected, actual);
+            Assert.assertEquals("Error Message should match",
+                    expected,
+                    actual);
         }
+
     }
 
     @Then("validate field error messages using map")
     public void validate_field_error_messages_using_map(DataTable dataTable) {
-        Map<String, String> expectedData = dataTable.asMap(String.class, String.class);
-        List<WebElement> errorElements = getElements(SignUpPage.ERROR_MESSAGE);
+        Map<String, String> expectedData = dataTable.asMap();
 
-        Assert.assertEquals("Number of error messages should be the same as expected", expectedData.size(), errorElements.size());
+        List<WebElement> errorElements = getElements(SignUpPage.ERROR_MESSAGE);
+        Assert.assertEquals("Number of Error message should be same as Expected",
+                expectedData.size(),
+                errorElements.size());
 
         Map<String, String> actualErrors = new HashMap<>();
-        for (WebElement element : errorElements) {
+        for(WebElement element : errorElements) {
             String text = element.getText();
             String key = text.split(" ")[0];
             actualErrors.put(key, text);
         }
 
         for (String key : expectedData.keySet()) {
-            Assert.assertEquals("Error message should match", expectedData.get(key), actualErrors.get(key));
+            Assert.assertEquals("Error Message should match",
+                    expectedData.get(key),
+                    actualErrors.get(key));
         }
+
+
+//        Assert.assertEquals("Error Message should match",
+//                expectedData.get("name"),
+//                errorElements.get(0).getText());
+//        Assert.assertEquals("Error Message should match",
+//                expectedData.get("email"),
+//                errorElements.get(1).getText());
+//        Assert.assertEquals("Error Message should match",
+//                expectedData.get("password"),
+//                errorElements.get(2).getText());
+//        Assert.assertEquals("Error Message should match",
+//                expectedData.get("confirm"),
+//                errorElements.get(3).getText());
+
     }
 }
