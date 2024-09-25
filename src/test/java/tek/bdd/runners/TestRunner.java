f713e10abdb67aa8b9cad2b952a4e5bdd83740a4
zianzahid <ziamustafa2000@hotmail.com>
2024-09-24 17:19:29 -0700

    final json report

--- src/test/java/tek/bdd/runners/TestRunner.java
+++ src/test/java/tek/bdd/runners/TestRunner.java
@@ -6,13 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "classpath:features/ThursdayActivity", // Directory for Feature file
+        features = "classpath:features", // Directory for Feature file
         glue = "tek.bdd.steps", //Package for all the steps
-        dryRun = true, //set to true to scan feature for unimplemented steps
-        tags = "@Smoke",
-        plugin = {
-                "html:target/cucumber_report/index.html"
-        }
+        dryRun = false, //set to true to scan feature for unimplemented steps
+        tags = "@Regression",
+        plugin = {        "html:target/cucumber_report/index.html",
+                "json:target/jsonReport/report.json"}
 )
 public class TestRunner {
 }
